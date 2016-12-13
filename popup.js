bbf1e2bbd70f2d8e2f93893c7b2f26fb16026fc0
Thomas Gorczynski <me@thomasgorczynski.com>
2016-12-13 12:45:29 -0600

    List of all the tabs in the console

--- popup.js
+++ popup.js
@@ -7,32 +7,16 @@
 
 
 
-
-// Do NOT forget that the method is ASYNCHRONOUS
-chrome.tabs.query({
-    active: true,               // Select active tabs
-    lastFocusedWindow: true     // In the current window
-}, function(array_of_Tabs) {
-    // Since there can only be one active tab in one active window, 
-    //  the array has only one element
-    var tab = array_of_Tabs[0];
-    // Example:
-    var url = tab.url;
-    // ... do something with url variable
-	
-	
-});
-
 chrome.tabs.executeScript({
     code: 'console.log("hi")'
 });
 
-chrome.tabs.query({
-    active: true,
-    currentWindow: true
-}, function(tabs) {
-    var tabURL = tabs[0].url;
-	chrome.tabs.executeScript({
-    code: 'console.log("hi there' + tabURL + '")'
+chrome.tabs.query({}, function(tabs) {
+	var arrayLength = tabs.length;
+for (var i = 0; i < arrayLength; i++) {
+    chrome.tabs.executeScript({
+    code: 'console.log("tab ' + i + ' has url ' + tabs[i].url + '")'
 });
+}
+	
 });
