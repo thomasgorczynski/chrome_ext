2a41544c93c32e4f821b8f0f8125209052f651e3
Thomas Gorczynski <me@thomasgorczynski.com>
2016-12-15 20:34:53 -0600

    Able to sort the tabs in order, select the list, and return the last/new position of the tab

--- popup.js
+++ popup.js
@@ -6,22 +6,41 @@
  */
 
 
-
 chrome.tabs.executeScript({
     code: 'console.log("hi")'
 });
 
 chrome.tabs.query({}, function(tabs) {
 	var arrayLength = tabs.length;
-	var text = "<ul>";
+	var text = "<ul id='listWithHandle'>";
 for (var i = 0; i < arrayLength; i++) {
     chrome.tabs.executeScript({
     code: 'console.log("tab ' + i + ' has url ' + tabs[i].url + '")'
 });
-	text += "<li><a href='" + tabs[i].url + "' target='_blank' >" + tabs[i].title + "</a></li>";
+	text += "<li><span class='my-handle'>::</span><a href='" + tabs[i].url + "' target='_blank' >" + tabs[i].title + "</a></li>";
 document.getElementById("tester").innerHTML = text;
 }
 	text += "</ul>";
 	
+	// List with handle
+Sortable.create(listWithHandle, {
+  handle: '.my-handle',
+  animation: 150
+,// Changed sorting within list
+    onEnd: function (/**Event*/evt) {
+        evt.oldIndex;  // element's old index within parent
+        evt.newIndex;  // element's new index within parent
+        chrome.tabs.executeScript({
+    code: 'console.log("tab is now ' +  evt.oldIndex + '")'
+});
+		}
+    });
+	
 });
 
+
+function hello() {
+  chrome.tabs.executeScript(alert('hello ' + document.location.href)); 
+}
+
+document.getElementById('clickme').addEventListener('click', hello);
